a266e74282e1a1466f94011f88d97f8f8c75ed6f
Arfan-Mulana <arfan_ibrahim_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:56:26 +0700

    Add files via upload

---
+++ TesGuru2.java
@@ -0,0 +1,76 @@
+public class TesGuru {
+    public static void main(String[] args) {
+        Guru aulia = new Guru();
+        Guru feni = new Guru();
+        Guru diaur = new Guru();
+        Guru pasha = new Guru();
+        Guru abidin = new Guru();
+
+        aulia.id = 1;
+        aulia.nama = "Aulia Mas'adah";
+        aulia.mapel = "Produktif RPL";
+        aulia.alamat = "Malang";
+        
+        feni.id = 2;
+        feni.nama = "Feniliya Mayrini";
+        feni.mapel = "Matematika";
+        feni.alamat = "Malang";
+
+        diaur.id = 3;
+        diaur.nama ="Diaur Rahman";
+        diaur.mapel = "IPAS";
+        diaur.alamat ="Ngawi";
+
+        pasha.id = 4;
+        pasha.nama ="Pashatania Fitri Indah Lestari";
+        pasha.mapel ="Produktif RPL";
+        pasha.alamat ="Malang";
+
+        abidin.id = 5;
+        abidin.nama ="Achmad Abidin";
+        abidin.mapel ="Matematika";
+        abidin.alamat ="Malang";
+
+        System.out.println(" Ini Data Bu Aulia");
+        System.out.println(aulia.id);
+        System.out.println(aulia.nama);
+        System.out.println(aulia.mapel);
+        System.out.println(aulia.alamat);
+
+        System.out.println(" Ini Data Bu Feni");
+        System.out.println(feni.id);
+        System.out.println(feni.nama);
+        System.out.println(aulia.mapel);
+        System.out.println(aulia.alamat);
+
+        System.out.println(" Ini Data Pak Diaur");
+        System.out.println(diaur.id);
+        System.out.println(diaur.nama);
+        System.out.println(aulia.mapel);
+        System.out.println(aulia.alamat);
+
+        System.out.println(" Ini Data Bu pasha");
+        System.out.println(pasha.id);
+        System.out.println(pasha.nama);
+        System.out.println(aulia.mapel);
+        System.out.println(aulia.alamat);
+
+        System.out.println(" Ini Data Pak Adidin");
+        System.out.println(abidin.id);
+        System.out.println(abidin.nama);
+        System.out.println(aulia.mapel);
+        System.out.println(aulia.alamat);
+
+
+
+
+        
+    
+
+
+
+
+
+
+    }
+}
